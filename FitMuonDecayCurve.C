c5f38fb943263c910ccd313986ca1e3656e0cf15
Nurfikri Norjoahruddeen <nurfikri89@gmail.com>
2023-03-05 18:12:14 +0900

    Minor fix

--- FitMuonDecayCurve.C
+++ FitMuonDecayCurve.C
@@ -37,7 +37,7 @@ void DoFit(TString runRange = "1530-1880"){
   float minFitRange = 0.;
   float maxFitRange = 8.5;
 
-  int totalEntries = muonDecayCurve->GetEntries();
+  int totalEntries = muonDecayCurve->Integral(0,muonDecayCurve->GetNbinsX()+1);
   
   TVirtualFitter::SetMaxIterations( 10000 );
   //
@@ -50,53 +50,53 @@ void DoFit(TString runRange = "1530-1880"){
   );
   SetLineCosmetics(fTotal, kRed+1, 2, 2);
   // 
-  SetParInfo(fTotal, 0, 250, 0, totalEntries*1.1, "Exp0Norm");
+  SetParInfo(fTotal, 0, 250, 0, totalEntries, "Exp0Norm");
   SetParInfo(fTotal, 1, 2.2, 0.2, 25, "Exp0Decay");
   //
-  SetParInfo(fTotal, 2, 50, 0, totalEntries*1.1, "Exp1Norm");
+  SetParInfo(fTotal, 2, 50, 0, totalEntries, "Exp1Norm");
   SetParInfo(fTotal, 3, 0.16, 0.02, 2.0, "Exp1Decay");
   // Gauss0
-  SetParInfo(fTotal, 4, 1000, 0., totalEntries*1.1, "Gaus0Norm");
+  SetParInfo(fTotal, 4, 1000, 0., totalEntries, "Gaus0Norm");
   SetParInfo(fTotal, 5, 1.1, 0.5,2.0, "Gaus0Mu");
   SetParInfo(fTotal, 6, 0.1, 0.,1., "Gaus0Sigma");
   // Gauss1
-  SetParInfo(fTotal, 7, 300, 0., totalEntries*1.1, "Gaus1Norm");
+  SetParInfo(fTotal, 7, 300, 0., totalEntries, "Gaus1Norm");
   SetParInfo(fTotal, 8, 1.7, 1.0, 2.5, "Gaus1Mu");
   SetParInfo(fTotal, 9, 0.1, 0.,1., "Gaus1Sigma");
   // Gauss2
-  SetParInfo(fTotal, 10, 500, 0., totalEntries*1.1, "Gaus2Norm");
+  SetParInfo(fTotal, 10, 500, 0., totalEntries, "Gaus2Norm");
   SetParInfo(fTotal, 11, 2.4, 1.5, 3.0, "Gaus2Mu");
   SetParInfo(fTotal, 12, 0.1, 0.,1., "Gaus2Sigma");
   // Gauss3
-  SetParInfo(fTotal, 13, 500, 0., totalEntries*1.1, "Gaus3Norm");
+  SetParInfo(fTotal, 13, 500, 0., totalEntries, "Gaus3Norm");
   SetParInfo(fTotal, 14, 2.9, 2.5, 3.5, "Gaus3Mu");
   SetParInfo(fTotal, 15, 0.1, 0.,1., "Gaus3Sigma");
   // Gauss4
-  SetParInfo(fTotal, 16, 200, 0., totalEntries*1.1, "Gaus4Norm");
+  SetParInfo(fTotal, 16, 200, 0., totalEntries, "Gaus4Norm");
   SetParInfo(fTotal, 17, 3.4, 3.0, 4.0, "Gaus4Mu");
   SetParInfo(fTotal, 18, 0.1, 0.,1., "Gaus4Sigma");
   // Gauss5
-  SetParInfo(fTotal, 19, 1000, 0., totalEntries*1.1, "Gaus5Norm");
+  SetParInfo(fTotal, 19, 1000, 0., totalEntries, "Gaus5Norm");
   SetParInfo(fTotal, 20, 4.2, 3.5, 4.5, "Gaus5Mu");
   SetParInfo(fTotal, 21, 0.1, 0.,1., "Gaus5Sigma");
   // Gauss6
-  SetParInfo(fTotal, 22, 900, 0., totalEntries*1.1, "Gaus6Norm");
+  SetParInfo(fTotal, 22, 900, 0., totalEntries, "Gaus6Norm");
   SetParInfo(fTotal, 23, 5.3, 4.5, 5.5, "Gaus6Mu");
   SetParInfo(fTotal, 24, 0.1, 0.,1., "Gaus6Sigma");
   // Gauss7
-  SetParInfo(fTotal, 25, 500, 0., totalEntries*1.1, "Gaus7Norm");
+  SetParInfo(fTotal, 25, 500, 0., totalEntries, "Gaus7Norm");
   SetParInfo(fTotal, 26, 6.4, 5.0, 7.0, "Gaus7Mu");
   SetParInfo(fTotal, 27, 0.1, 0.,1., "Gaus7Sigma");
   // Gauss8
-  SetParInfo(fTotal, 28, 500, 0., totalEntries*1.1, "Gaus8Norm");
+  SetParInfo(fTotal, 28, 500, 0., totalEntries, "Gaus8Norm");
   SetParInfo(fTotal, 29, 7.0, 6.5, 7.5, "Gaus8Mu");
   SetParInfo(fTotal, 30, 0.1, 0.,1., "Gaus8Sigma");
   // Gauss9
-  SetParInfo(fTotal, 31, 600, 0., totalEntries*1.1, "Gaus9Norm");
+  SetParInfo(fTotal, 31, 600, 0., totalEntries, "Gaus9Norm");
   SetParInfo(fTotal, 32, 7.5, 7.0, 8.0, "Gaus9Mu");
   SetParInfo(fTotal, 33, 0.1, 0.,1., "Gaus9Sigma");
   // Gauss10
-  SetParInfo(fTotal, 34, 600, 0., totalEntries*1.1, "Gaus10Norm");
+  SetParInfo(fTotal, 34, 600, 0., totalEntries, "Gaus10Norm");
   SetParInfo(fTotal, 35, 8.2, 7.5, 8.5, "Gaus10Mu");
   SetParInfo(fTotal, 36, 0.1, 0.,1., "Gaus10Sigma");
   // pol0
